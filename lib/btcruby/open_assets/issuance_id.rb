9663628a940be9b9d383a0cebd9c0ae2d1531b39
Oleg Andreev <oleganza@gmail.com>
2015-08-26 16:08:51 +0200

    removed unnecessary amount from IssuanceID definition

--- lib/btcruby/open_assets/issuance_id.rb
+++ lib/btcruby/open_assets/issuance_id.rb
@@ -13,11 +13,9 @@ module BTC
       125 # 's' prefix
     end
 
-    def initialize(string: nil, hash: nil, network: nil, outpoint: nil, amount: nil, _raw_data: nil)
-      if outpoint || amount
-        raise ArgumentError, "Outpoint is missing" if !outpoint
-        raise ArgumentError, "Amount is missing" if !amount || amount < 0
-        data = outpoint.transaction_hash + WireFormat.encode_uint32be(outpoint.index) + WireFormat.encode_uint64le(amount) 
+    def initialize(string: nil, hash: nil, network: nil, outpoint: nil, _raw_data: nil)
+      if outpoint
+        data = outpoint.transaction_hash + WireFormat.encode_uint32be(outpoint.index)
         super(hash: BTC.hash160(data), network: network)
       else
         super(string: string, hash: hash, network: network, _raw_data: _raw_data)
--- spec/open_assets/issuance_id_spec.rb
+++ spec/open_assets/issuance_id_spec.rb
@@ -3,14 +3,14 @@ require_relative '../spec_helper'
 describe BTC::IssuanceID do
   it "should encode script to a correct address" do
     key = BTC::Key.new(private_key: "18E14A7B6A307F426A94F8114701E7C8E774E7F9A47E2C2035DB29A206321725".from_hex, public_key_compressed: false)
-    issuance_id = BTC::IssuanceID.new(outpoint: BTC::Outpoint.new(transaction_hash: BTC.hash256("tx1"), index:1), amount:100, network: BTC::Network.mainnet)
-    issuance_id.hash.to_hex.must_equal "29d2765469a299d1219a1cbf7561534eae4595f7"
-    issuance_id.to_s.must_equal "SR78rsv5RCMMay22D3ZUZmMgPkRGpqphnQ"
+    issuance_id = BTC::IssuanceID.new(outpoint: BTC::Outpoint.new(transaction_hash: BTC.hash256("tx1"), index:1), network: BTC::Network.mainnet)
+    issuance_id.hash.to_hex.must_equal "601a635e4f95178e999b9957cc8cea255e988a4f"
+    issuance_id.to_s.must_equal "SW49WjotbWmG4GMWXZmpP3bdCrABmC8LRG"
     issuance_id.is_a?(BTC::IssuanceID).must_equal true
   end
   it "should decode an asset address" do
-    issuance_id = BTC::Address.parse("SR78rsv5RCMMay22D3ZUZmMgPkRGpqphnQ")
+    issuance_id = BTC::Address.parse("SW49WjotbWmG4GMWXZmpP3bdCrABmC8LRG")
     issuance_id.is_a?(BTC::IssuanceID).must_equal true
-    issuance_id.hash.to_hex.must_equal "29d2765469a299d1219a1cbf7561534eae4595f7"
+    issuance_id.hash.to_hex.must_equal "601a635e4f95178e999b9957cc8cea255e988a4f"
   end
 end
